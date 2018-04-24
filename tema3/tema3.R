b3964d6bc0242fb1c66bd934ff9912252909ef93
Andrei-Iulian Juravle <andrei.juravle@info.uaic.ro>
2018-04-24 20:38:26 +0300

    Random forest

--- tema3/tema3.R
+++ tema3/tema3.R
@@ -4,7 +4,33 @@ library(RColorBrewer)
 library(e1071)
 library(class)
 
-train <- read.csv("input_files/train.csv")
-test <- read.csv("input_files/test.csv")
+train <- read.csv("../input_files/train.csv")
+test <- read.csv("../input_files/test.csv")
 
-knn_titanic <-knn(train[, -1], test[, -1], train[, 1], k = 5)
+# Remove Name, Ticket and Cabin columns
+train <- train[,-c(4,9,11)]
+test <- test[,-c(3,8,10)]
+
+# Change Sex to 0 = male, 1 = female
+train$Sex <- sapply(as.character(train$Sex), switch, 'male' = 0, 'female' = 1)
+test$Sex <- sapply(as.character(test$Sex), switch, 'male' = 0, 'female' = 1)
+
+# Change Embarked column to 0 = 'C', 1 = 'Q', 2 = 'S' and remove NAs
+train$Embarked[train$Embarked == ''] <- 'S'
+train$Embarked <- sapply(as.character(train$Embarked), switch, 'C' = 0, 'Q' = 1, 'S' = 2)
+test$Embarked <- sapply(as.character(test$Embarked), switch, 'C' = 0, 'Q' = 1, 'S' = 2)
+
+# Remove NAs from Age and Fare columns
+train_age <- na.omit(train$Age)
+train_age_avg <- mean(train_age)
+train$Age[is.na(train$Age)] <- train_age_avg
+
+test_age <- na.omit(test$Age)
+test_age_avg <- mean(test_age)
+test$Age[is.na(test$Age)] <- test_age_avg
+
+test_fare <- na.omit(test$Fare)
+test_fare_avg <- mean(test_fare)
+test$Fare[is.na(test$Fare)] <- test_fare_avg
+
+#random_forest <- randomForest(train[,-c(1,2)])
