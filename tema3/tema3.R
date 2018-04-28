a347202aec7f755c66ed8fcd59d94f38fba313dd
Andrei-Iulian Juravle <andrei.juravle@info.uaic.ro>
2018-04-28 23:00:22 +0300

    Tema3 - gata

--- tema3/tema3.R
+++ tema3/tema3.R
@@ -1,48 +1,16 @@
-library(rattle)
-library(rpart.plot)
-library(RColorBrewer)
-library(e1071)
-library(class)
-library(randomForest)
-library(xgboost)
-library('dplyr')
-
-train <- read.csv("input_files/train.csv")
-test <- read.csv("input_files/test.csv")
-
-full  <- bind_rows(train, test)
-
-# Extract title from name
-full$Title <- gsub('(.*, )|(\\..*)', '', full$Name)
-rare_title <- c('Dona', 'Lady', 'the Countess','Capt', 'Col', 'Don', 
-                'Dr', 'Major', 'Rev', 'Sir', 'Jonkheer')
-full$Title[full$Title == 'Mlle']        <- 'Miss' 
-full$Title[full$Title == 'Ms']          <- 'Miss'
-full$Title[full$Title == 'Mme']         <- 'Mrs' 
-full$Title[full$Title %in% rare_title]  <- 'Rare Title'
-
-# Extract Surname from Name
-full$Surname <- sapply(full$Name,  
-                       function(x) strsplit(as.character(x), split = '[,.]')[[1]][1])
-
-# Create a family size variable including the passenger themselves
-full$Fsize <- full$SibSp + full$Parch + 1
-
-# Create a family variable 
-full$Family <- paste(full$Surname, full$Fsize, sep='_')
-
-# Discretize family size
-full$FsizeD[full$Fsize == 1] <- 'singleton'
-full$FsizeD[full$Fsize < 5 & full$Fsize > 1] <- 'small'
-full$FsizeD[full$Fsize > 4] <- 'large'
-
-# Create a Deck variable. Get passenger deck A - F:
-full$Deck<-factor(sapply(full$Cabin, function(x) strsplit(as.character(x), NULL)[[1]][1]))
-
-full$Child[full$Age < 18] <- 'Child'
-full$Child[full$Age >= 18] <- 'Adult'
-full$Mother <- 'Not Mother'
-full$Mother[full$Sex == 'female' & full$Parch > 0 & full$Age > 18 & full$Title != 'Miss'] <- 'Mother'
-
-train <- full[1:891,]
-test <- full[892:1309,]
+rez = c(150)
+for (j in 1:150) {
+  accuracies <- c()
+  folds <- createFolds(train$Survived, k = 5, list = TRUE, returnTrain = TRUE)
+  for (i in 1:5) {
+    model <- rpart(Survived ~ Pclass + Sex + Age + SibSp + Parch + Fare + Embarked + Title + Fsize, 
+                   data = train[folds[[i]],], method = "class", 
+                   control=rpart.control(minsplit=124, cp=0.0078, maxcompete=113))
+    prediction <- predict(object = model, newdata = train[-folds[[i]],], type = "class")
+    accuracies <- c(accuracies, confusionMatrix(prediction, as.factor(train[-folds[[i]], ]$Survived))$overall[[1]])
+  }
+  rez[j] = mean(accuracies)
+}
+plot(rez)
+which(rez==max(rez)) 
+rez[which(rez==max(rez))]
